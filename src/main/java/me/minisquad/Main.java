49c2e7cca30306b1b95ab361e59a97881ae4f71c
Bruce How <brucehow4@gmail.com>
2019-06-10 22:35:35 +0800

    Initial commit

---
+++ src/main/java/me/minisquad/Main.java
@@ -0,0 +1,38 @@
+package me.minisquad;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+
+/**
+ * Main class for the CITS bot discord bot
+ */
+
+ public class Main {
+    private static JDA jda;
+
+    public static void main(String[] args) {
+        try {
+            jda = new JDABuilder(Constants.BOT_TOKEN).build();
+            jda.getPresence().setStatus(OnlineStatus.ONLINE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Output consistent logging with data timestamps
+     * @param log String to output
+     */
+    public static void output(String log) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Australia/Perth"));
+        Date date = new Date();
+        System.out.println(dateFormat.format(date) + " " + log);
+    }
+ }
